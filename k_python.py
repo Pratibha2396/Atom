f426434f924cdcfb3e54293ee901d809e27b46a1
Pratibha2396 <69852087+Pratibha2396@users.noreply.github.com>
2022-08-29 16:14:12 +0530

    Update k_python.py

--- k_python.py
+++ k_python.py
@@ -10,7 +10,7 @@ import json
 from datetime import datetime
 
 if __name__ == '__main__':
-    producer = KafkaProducer(bootstrap_servers = ['ip-172-31-81-238.ec2.internal:9092'], api_version =(0,10,1))
+    producer = KafkaProducer(bootstrap_servers = ['172.31.81.238:9092'], api_version =(0,10,1))
     for i in range(50):
         message = "message {}".format(str(datetime.now().time()))
         producer.send('log record',json.dumps(message).encode('utf-8'))
