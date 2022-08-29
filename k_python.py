2e5726dc7e94046d84c2d228e642aa9648e07f82
Pratibha2396 <69852087+Pratibha2396@users.noreply.github.com>
2022-08-29 12:35:47 +0530

    Add files via upload

---
+++ k_python.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from kafka import KafkaProducer
+from time import sleep
+import json
+from datetime import datetime
+
+if __name__ == '__main__':
+    producer = KafkaProducer(bootstrap_servers = ['localhost:9092'], api_version =(0,10,1))
+    for i in range(50):
+        message = "message {}".format(str(datetime.now().time()))
+        producer.send('log record',json.dumps(message).encode('utf-8'))
+        sleep(2)
+        print('Message sent', i)
+
